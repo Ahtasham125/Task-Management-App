3fdc0af2801fa24981e20f57610235c7ec1c5c30
Ahtasham Khalid <ahtsham.fiverr045@gmail.com>
2025-05-31 10:57:56 +0500

    Add files via upload
    Task Management App using C# + SQL

---
+++ Task Management App/Task Management App/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Task_Management_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.dtpDueDate = new System.Windows.Forms.DateTimePicker();
+            this.cmbPriority = new System.Windows.Forms.ComboBox();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(327, 83);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(198, 20);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(327, 125);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(198, 20);
+            this.txtDescription.TabIndex = 2;
+            // 
+            // dtpDueDate
+            // 
+            this.dtpDueDate.Location = new System.Drawing.Point(327, 164);
+            this.dtpDueDate.Name = "dtpDueDate";
+            this.dtpDueDate.Size = new System.Drawing.Size(198, 20);
+            this.dtpDueDate.TabIndex = 3;
+            // 
+            // cmbPriority
+            // 
+            this.cmbPriority.FormattingEnabled = true;
+            this.cmbPriority.Location = new System.Drawing.Point(327, 207);
+            this.cmbPriority.Name = "cmbPriority";
+            this.cmbPriority.Size = new System.Drawing.Size(198, 21);
+            this.cmbPriority.TabIndex = 4;
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Location = new System.Drawing.Point(327, 255);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(198, 21);
+            this.cmbStatus.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-3, 389);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(669, 227);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(153, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(353, 37);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Task Management System ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(121, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Title";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(121, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Description";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(120, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 21);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Due Date";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(121, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 21);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Priority";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(125, 257);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 21);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Status";
+            // 
+            // btnadd
+            // 
+            this.btnadd.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnadd.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadd.Location = new System.Drawing.Point(90, 340);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(108, 33);
+            this.btnadd.TabIndex = 13;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = false;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click_1);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnupdate.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.Location = new System.Drawing.Point(288, 340);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(108, 33);
+            this.btnupdate.TabIndex = 14;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = false;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click_1);
+            // 
+            // btndelete
+            // 
+            this.btndelete.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btndelete.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.Location = new System.Drawing.Point(493, 340);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(108, 33);
+            this.btndelete.TabIndex = 15;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = false;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(664, 616);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.cmbPriority);
+            this.Controls.Add(this.dtpDueDate);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.txtTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.DateTimePicker dtpDueDate;
+        private System.Windows.Forms.ComboBox cmbPriority;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndelete;
+    }
+}
+
---
+++ Task Management App/Task Management App/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_Management_App
+{
+    public partial class Form1: Form
+    {
+        string connectionString = "Data Source=DESKTOP-H2KNIQ4\\SQLEXPRESS;Initial Catalog=TaskManagerDB;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadTasks();
+        }
+
+        private void LoadTasks()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Tasks", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Tasks (Title, Description, DueDate, Priority, Status) VALUES (@Title, @Desc, @Due, @Priority, @Status)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Title", txtTitle.Text);
+                cmd.Parameters.AddWithValue("@Desc", txtDescription.Text);
+                cmd.Parameters.AddWithValue("@Due", dtpDueDate.Value.Date);
+                cmd.Parameters.AddWithValue("@Priority", cmbPriority.Text);
+                cmd.Parameters.AddWithValue("@Status", cmbStatus.Text);
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            LoadTasks();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int taskId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "UPDATE Tasks SET Title=@Title, Description=@Desc, DueDate=@Due, Priority=@Priority, Status=@Status WHERE Id=@Id";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Title", txtTitle.Text);
+                    cmd.Parameters.AddWithValue("@Desc", txtDescription.Text);
+                    cmd.Parameters.AddWithValue("@Due", dtpDueDate.Value.Date);
+                    cmd.Parameters.AddWithValue("@Priority", cmbPriority.Text);
+                    cmd.Parameters.AddWithValue("@Status", cmbStatus.Text);
+                    cmd.Parameters.AddWithValue("@Id", taskId);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                LoadTasks();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int taskId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "DELETE FROM Tasks WHERE Id=@Id";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Id", taskId);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                LoadTasks();
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow row = dataGridView1.SelectedRows[0];
+                txtTitle.Text = row.Cells["Title"].Value.ToString();
+                txtDescription.Text = row.Cells["Description"].Value.ToString();
+                dtpDueDate.Value = Convert.ToDateTime(row.Cells["DueDate"].Value);
+                cmbPriority.Text = row.Cells["Priority"].Value.ToString();
+                cmbStatus.Text = row.Cells["Status"].Value.ToString();
+            }
+        }
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbPriority.Items.AddRange(new string[] { "Low", "Medium", "High" });
+            cmbStatus.Items.AddRange(new string[] { "Pending", "In Progress", "Done" });
+        }
+
+        private void btnadd_Click_1(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Tasks (Title, Description, DueDate, Priority, Status) VALUES (@Title, @Desc, @Due, @Priority, @Status)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Title", txtTitle.Text);
+                cmd.Parameters.AddWithValue("@Desc", txtDescription.Text);
+                cmd.Parameters.AddWithValue("@Due", dtpDueDate.Value.Date);
+                cmd.Parameters.AddWithValue("@Priority", cmbPriority.Text);
+                cmd.Parameters.AddWithValue("@Status", cmbStatus.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            LoadTasks(); // refresh DataGridView
+        }
+
+        private void btnupdate_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int taskId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "UPDATE Tasks SET Title=@Title, Description=@Desc, DueDate=@Due, Priority=@Priority, Status=@Status WHERE Id=@Id";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Title", txtTitle.Text);
+                    cmd.Parameters.AddWithValue("@Desc", txtDescription.Text);
+                    cmd.Parameters.AddWithValue("@Due", dtpDueDate.Value.Date);
+                    cmd.Parameters.AddWithValue("@Priority", cmbPriority.Text);
+                    cmd.Parameters.AddWithValue("@Status", cmbStatus.Text);
+                    cmd.Parameters.AddWithValue("@Id", taskId);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                LoadTasks();
+            }
+        }
+
+        private void btndelete_Click_1(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                int taskId = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "DELETE FROM Tasks WHERE Id=@Id";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Id", taskId);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                LoadTasks();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow row = dataGridView1.SelectedRows[0];
+                txtTitle.Text = row.Cells["Title"].Value.ToString();
+                txtDescription.Text = row.Cells["Description"].Value.ToString();
+                dtpDueDate.Value = Convert.ToDateTime(row.Cells["DueDate"].Value);
+                cmbPriority.Text = row.Cells["Priority"].Value.ToString();
+                cmbStatus.Text = row.Cells["Status"].Value.ToString();
+            }
+        }
+    }
+}
